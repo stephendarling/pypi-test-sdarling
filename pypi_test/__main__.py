40d85a30253c674ea78e7bbb7917e48074f307d3
Stephen Darling <stephen.darling@outlook.com>
2019-01-08 03:35:44 -0500

    Added utilities sub-module

--- pypi_test/__main__.py
+++ pypi_test/__main__.py
@@ -1,5 +1,8 @@
+import lib.utilities as utilities
+
 def main():
-  print('This is the main function')
+  total = utilities.add_two_values(1, 9)
+  print(f'The total is {total}')
   
 if __name__ == "__main__":
   main()
